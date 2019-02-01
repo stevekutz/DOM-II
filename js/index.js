7364934204e5eae1c699d7cffcd6812d314a59bd
stevekutz <stkutz@gmail.com>
2019-01-31 23:31:37 -0600

    added centered hover effect on nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const anchors = document.querySelectorAll('a');
+console.log(anchors);
+
+
+// hover effect
+anchors.forEach(a => {
+  a.addEventListener('mouseover',
+      ev => {ev.target.style.color = 'dodgerblue';
+             ev.target.style.fontSize = '3rem'
+                                                  }
+  );
+  a.addEventListener('mouseleave',
+        ev => {ev.target.style.color = '#212529';
+               ev.target.style.fontSize = '1.6rem'}
+  );
+});
+
+
+// change background to random 'nice' color
+
