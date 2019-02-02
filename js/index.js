26b587fba11bf8ff0283cd3a6814cfb4ec3fa64c
stevekutz <stkutz@gmail.com>
2019-02-02 00:39:31 -0600

    added dblclick toggle background color toggle on body

--- js/index.js
+++ js/index.js
@@ -17,5 +17,34 @@ anchors.forEach(a => {
 });
 
 
-// change background to random 'nice' color
+// change background to random 'nice' color OR toggle this!!!
 
+let toggleClosure = () => {
+  let state = true;
+
+  function toggleState () {
+    state = !state;
+    return `state is now ${state}`;
+  }
+
+  return toggleState;
+};
+
+let toggler = toggleClosure();
+console.log('t first ', toggler());
+console.log('t second', toggler());
+
+const body = document.querySelector('body');
+console.log(body);
+
+let state = true;
+
+body.addEventListener('dblclick',
+  ev => {
+   state ? body.style.backgroundColor = 'yellow'
+               : body.style.backgroundColor = 'pink';
+
+   state = !state;
+
+  }
+);
