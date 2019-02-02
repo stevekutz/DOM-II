e8abd0cd620b0a8fffa6f300d623666a46d33b01
stevekutz <stkutz@gmail.com>
2019-02-02 13:18:56 -0600

    MVP complete + STRETCH

--- js/index.js
+++ js/index.js
@@ -17,16 +17,35 @@ nav.setAttribute('align-content', 'center');
 */
 
 // hover effect
+const header = document.querySelector('header');
+
+
+// hover effect
+// NOTE !!
+// WOW, interesting behavior on how these fight with each other !!!
+
+
 anchors.forEach(a => {
   a.addEventListener('mouseover',
-      ev => {ev.target.style.color = 'dodgerblue';
-             ev.target.style.fontSize = '3rem'
-                                                  }
+    ev => {ev.target.style.color = 'dodgerblue';
+      ev.target.style.fontSize = '3rem'
+    }
   );
+
+  a.addEventListener('focus',
+      ev => {header.style.backgroundColor = 'deeppink'}
+    );
+
   a.addEventListener('mouseleave',
-        ev => {ev.target.style.color = '#212529';
-               ev.target.style.fontSize = '1.6rem'}
+    ev => {ev.target.style.color = '#212529';
+      ev.target.style.fontSize = '1.6rem'
+  }
   );
+
+  a.addEventListener('blur',
+    ev => {header.style.backgroundColor = 'grey'}
+  );
+
 });
 
 
@@ -81,14 +100,13 @@ window.addEventListener('resize', ev => {
 
 });
 
-
-/*   // DEBUGGING, verifying bubbling concept
+////////////////////    NESTED BUBBLING
 funBus_h1.addEventListener('dblclick', ev => {
   console.log('funBus_h1 clicked  ', funBus_h1);
   // YES,  this bubbles up ALL the WAY to document !!
   ev.stopPropagation();   // THIS Stops bubbling
 } );
-*/
+
 
 // making scroll happen
 const welcome_h2 = document.querySelector('h2'); // should be first h2
@@ -100,10 +118,30 @@ window.addEventListener('scroll', ev => {
 
 });
 
+// making wheel happen
+const letsGo = document.querySelector('.text-content h2');
+console.log('letsGo is ', letsGo);
+window.addEventListener('wheel', ev => {
+  count_scroll++;
+  console.log('welcome_h2 scolled ', count_scroll);
+  letsGo.textContent = `Welcome to Fun Bus! ${count_scroll} wheels`
 
 
+});
+
+//  background restore with a single click
+// NOTE !!!    alert breaks this !!!
+body.addEventListener('click', ev => {
+  console.log('single click on page');
+  // alert('back to normal, whew!! ');
+  body.style.backgroundColor = 'white';
+//  header.style.backgroundColor = 'white';   // interesting stuff here too!
+  // alert('back to normal, whew!! ');
+  });
 
 
+// keydown
+body.addEventListener('keypress', ev => alert('key was pressed'));
 
 
 // STRETCH   Fix for ALL buttons
